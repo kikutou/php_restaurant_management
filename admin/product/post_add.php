26f2db405ff71e643505bfd0db8c8c236df2d452
Sterne1120 <shuushingyoku@MacBook-Pro.local>
2019-04-10 16:43:24 +0900

    revision

--- admin/product/post_add.php
+++ admin/product/post_add.php
@@ -7,7 +7,6 @@
 <body>
 
 <?php
-
 //データ収集とバリデーション
 $error_messages = Product::validate($_POST);
 if(count($error_messages) > 0) {
@@ -18,12 +17,59 @@ if(count($error_messages) > 0) {
   exit();
 }
 
+//上传文件代码
+// 允许上传的图片后缀
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+$temp = explode(".", $_FILES["picture"]["name"]);
+$extension = end($temp);     // 获取文件后缀名
+if ((($_FILES["picture"]["type"] == "image/gif")
+|| ($_FILES["picture"]["type"] == "image/jpeg")
+|| ($_FILES["picture"]["type"] == "image/jpg")
+|| ($_FILES["picture"]["type"] == "image/pjpeg")
+|| ($_FILES["picture"]["type"] == "image/x-png")
+|| ($_FILES["picture"]["type"] == "image/png"))
+&& ($_FILES["picture"]["size"] < 2048000000)   // 小于 200 kb
+&& in_array($extension, $allowedExts))
+{
+    if ($_FILES["picture"]["error"] > 0)
+    {
+        echo "错误：: " . $_FILES["picture"]["error"] . "<br>";
+        exit();
+    }
+    else
+    {
+
+        // 判断当期目录下的 upload 目录是否存在该文件
+        // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
+        if (file_exists("../../asset/admin/img/upload/" . $_FILES["picture"]["name"]))
+        {
+            echo $_FILES["picture"]["name"] . " 文件已经存在。 ";
+            exit();
+        }
+        else
+        {
+            // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
+            move_uploaded_file($_FILES["picture"]["tmp_name"], "../../asset/admin/img/upload/" . $_FILES["picture"]["name"]);
+            $picture_path = "../../asset/admin/img/upload/" . $_FILES["picture"]["name"];
+        }
+    }
+}
+else
+{
+    echo "非法的文件格式";
+    exit();
+}
+
+// <!--END 上传文件代码--!>
+
+
 $products = new Product();
 //add_function
 $products->name = $_POST["name"];
 $products->price = $_POST["price"];
 $products->instruction= $_POST["instruction"];
-$products->picture= $_POST["picture"];
+$products->category_id= $_POST["category_id"];
+$products->picture = $picture_path;
 $products->created_at = date("Y-m-d H:i:s");
 $products->updated_at = date("Y-m-d H:i:s");
 $result = $products->save();
@@ -35,6 +81,6 @@ if($result) {
 
  ?>
 
-<a href='get_index.php'>戻る</a></br>
+<a href='get_index.php' class="button">戻る</a></br>
 </body>
 </html>
