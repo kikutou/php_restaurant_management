04e94bad4b61edccec40813ff3bec0d6efc4988d
chienyinyu <deidara036@gmail.com>
2019-04-16 18:42:41 +0900

    chien chien

--- customer/get_products_index.php
+++ customer/get_products_index.php
@@ -7,7 +7,7 @@ session_start();
 <html>
 <head>
 <meta charset="UTF-8">
-<title><?= Category::find($_GET["id"])->name ?>のメニュー</title>
+<title><?= Category::find($_GET["category_id"])->name ?>のメニュー</title>
 <link rel="stylesheet" type="text/css" href="style.css" charset="utf-8">
 <link href="https://w3g.jp/sample/css/font-family" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,8 +17,8 @@ session_start();
 <script src="https://cdn.staticfile.org/twitter-bootstrap/4.1.0/js/bootstrap.min.js"></script>
 </head>
 <body align="center">
-  <div class="jumbotron text-center has-border">
-    <h1><?= Category::find($_GET["id"])->name ?></h1>
+  <div class="jumbotron text-center">
+    <h1><?= Category::find($_GET["category_id"])->name ?></h1>
   </div>
   <div class="container">
     <div class="row">
@@ -26,17 +26,17 @@ session_start();
       $products = Product::get();
       $product_in_this_category=array();
       foreach($products as $product) {
-        if($product->category_id == $_GET["id"]){
+        if($product->category_id == $_GET["category_id"]){
           $products_in_this_category[] = $product;
         }
       };
 
       foreach($products_in_this_category as $product_in_this_category) : ?>
-        <div class="col-4 text-center has-border">
-          <a href="get_product_detail.php?id=<?=$product_in_this_category->id ?>">
-            <img src="<?php echo "hello/".$product_in_this_category->picture;?>" width="200" height="200" alt="<?= $product_in_this_category->picture ?>">
+        <div class="col-4 text-center">
+          <a href="get_product_detail.php?product_id=<?=$product_in_this_category->id ?>">
+            <img src="<?php echo "image/".$product_in_this_category->picture;?>" width="200" height="200" alt="<?= $product_in_this_category->picture ?>">
           </a><br>
-          <p><a href="get_product_detail.php?id=<?=$product_in_this_category->id ?>">
+          <p><a href="get_product_detail.php?product_id=<?=$product_in_this_category->id ?>">
              <?php echo $product_in_this_category->name; ?></a>
           </p>
           <p><?php echo $product_in_this_category->price; ?></p>
@@ -56,7 +56,7 @@ session_start();
    <div align="center" class="footer">
      <div align="right">
        <a href="shopping_cart.php">
-       <img src="../asset/customer/img/shoppingcart.png" width="100" height="100" alt="shoppingcart">
+       <img src="../asset/customer/img/shoppingcart.png" width="100" height="100" alt="shoppingcart"></a>
      </div>
      <p>&copy;Copyright Japan YaMii group. All rights reserved.</p>
    </div>
